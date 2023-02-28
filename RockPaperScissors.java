72d16b84b324362f82e2df9487934412b5f8c7e1
VukadinovP <122832250+VukadinovP@users.noreply.github.com>
2023-02-28 16:24:47 +0200

    Added main constuction

---
+++ RockPaperScissors.java
@@ -0,0 +1,10 @@
+public class RockPaperScissors {
+    private static final String ROCK = "Rock";
+    private static final String PAPER = "Paper";
+    private static final String SCISSORS = "Scissors";
+
+    public static void main(String[] args) {
+        System.out.print("Choose [r]ock, [p]aper, [s]cissors: ");
+
+    }
+}
